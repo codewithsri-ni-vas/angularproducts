fa2a18380c379c289e651dfc003943293516aa72
sri-ni-vas <srinivas.ibo.58@gmail.com>
2022-03-05 13:38:35 -0800

    6. template url vs template

--- ang-products/src/app/components/product-a/product-a.component.ts
+++ ang-products/src/app/components/product-a/product-a.component.ts
@@ -2,8 +2,12 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-product-a',
-  templateUrl: './product-a.component.html',
+  // templateUrl: './product-a.component.html',
+  template:`
+  <p>New Product A !</p>
+  `,
   styleUrls: ['./product-a.component.css']
+ 
 })
 export class ProductAComponent implements OnInit {
 
